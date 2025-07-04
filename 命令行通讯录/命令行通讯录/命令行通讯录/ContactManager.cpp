ac6735eabbe09bec3d7d1c4444da212bc44cc131
一一 <147343797@qq.com>
2025-07-05 00:02:32 +0800

    增加一些基础功能

--- 命令行通讯录/命令行通讯录/命令行通讯录/ContactManager.cpp
+++ 命令行通讯录/命令行通讯录/命令行通讯录/ContactManager.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <cctype>
 #include "ContactManager.h"
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -33,4 +35,74 @@ void ContactManager::addContact(const Contact& contact) {
 	}
 	contacts_.push_back(contact);
 	cout << "联系人添加成功" << endl;
+}
+//删除联系人
+bool ContactManager::deleteContact(const std::string& name) {
+	auto it = findContactByName(name);
+	if (it == contacts_.end()) {
+		cout << "未找到联系人" << endl;
+		return false;
+	}
+	contacts_.erase(it);
+	cout << "联系人删除成功！"<<endl;
+	return true;
+}
+
+//显示所有联系人
+void ContactManager:: displayAllContact()const {
+	if (contacts_.empty()) {
+		cout << "，没有联系人可以显示" << endl;
+		return;
+	}
+	cout << "\n---所有联系人（" << contacts_.size() << ")---" << endl;
+	for (const auto& contact : contacts_) {
+		contact.display();
+	}
+	cout << "----------------" << endl;
+
+}
+//保存到文件
+void ContactManager::saveToFile(const std::string& filename)const {
+	ofstream outFile(filename);
+	if (!outFile) {
+		throw std::runtime_error("无法打开文件：" + filename);
+	}
+	for (const auto& contact : contacts_) {
+		outFile << contact.getName() << ","
+			<< contact.getPhone() << ","
+			<< contact.getEmail() << ","
+			<< contact.getAddress() << "\n";
+
+	}
+	outFile.close();
+	cout << "联系人保存到文件" << filename <<endl;
+
+}
+//从文件加载
+
+void ContactManager::loadFromFile(const std::string& filename) {
+	ifstream inFile(filename);
+	if (!inFile) {
+		throw std::runtime_error("无法打开文件:" + filename);
+	}
+	contacts_.clear();
+
+	std::string line;
+	while (getline(inFile, line)) {
+		if (line.empty())continue;
+
+		std::istringstream ss(line);
+		std::string name, phone, email, address;
+		if (!getline(ss, name, ','))continue;
+		if (!getline(ss, phone, ','))continue;
+		if (!getline(ss, email, ','))continue;
+		getline(ss, address);
+		contacts_.emplace_back(name, phone, email, address);
+
+	}
+	inFile.close();
+	cout << "从文件加载的联系人：" << filename << "(" << contacts_.size() << "联系人）"
+		<<endl;
+
+
 }
